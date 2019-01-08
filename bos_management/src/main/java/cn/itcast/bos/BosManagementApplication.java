7c2f9b199ffe4c07db463650b94e8477dd440f8a
1158229122 <1158229122@qq.com>
2019-01-08 11:51:45 +0800

    bos物流项目初始化项目构建

---
+++ bos_management/src/main/java/cn/itcast/bos/BosManagementApplication.java
@@ -0,0 +1,22 @@
+package cn.itcast.bos;
+
+
+import cn.itcast.bos.domain.base.Area;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@SpringBootApplication
+@EntityScan("cn.itcast.bos.domain.base")//扫描实体类
+public class BosManagementApplication {
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(BosManagementApplication.class, args);
+    }
+}
